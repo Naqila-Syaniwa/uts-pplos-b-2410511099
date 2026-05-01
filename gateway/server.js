47ad0cc9e91124d1f85369d0d4157c1c559a9e6a
Naqila <nasyaaa312@gmail.com>
2026-05-01 22:51:51 +0700

    feat: implement api gateway with routing, jwt validation, and rate limiting

---
+++ gateway/server.js
@@ -0,0 +1,75 @@
+const express = require ("express");
+const { createProxyMiddleware } = require("http-proxy-middleware");
+const rateLimit = require("express-rate-limit");
+const jwt = require("jsonwebtoken");
+
+const app = express();
+
+// Rate limiting (60 req/min)
+const limiter = rateLimit({
+    windowMs: 60 * 1000,
+    max: 60,
+    message: "Too many requests, please try again later."
+});
+
+app.use(limiter);
+
+// JWT Validation Middleware
+const verifyToken = (req, res, next) => {
+    if (req.path.startsWith("/auth")) {
+        return next();
+    }
+
+    const authHeader = req.headers["authorization"];
+
+    if (!authHeader) {
+        return res.status(401).json({ message: "Token required" });
+    }
+
+    const token = authHeader.split(" ")[1];
+
+    jwt.verify(token, "SECRET_KEY", (err, decoded) => {
+        if (err) {
+            return res.status(403).json({ message: "Token required" });
+        }
+
+        req.user = decoded;
+        next();
+    });
+};
+
+app.use(verifyToken);
+
+// Routing ke auth, field, booking
+app.use(
+    "/auth", 
+    createProxyMiddleware({
+        target: "http://localhost:3001",
+        changeOrigin: true,
+    })
+);
+
+app.use(
+    "/fields",
+    createProxyMiddleware({
+        target: "http://localhost:8000",
+        changeOrigin: true,
+    })
+);
+
+app.use(
+    "/bookings",
+    createProxyMiddleware({
+        target: "http://localhost:3003",
+        changeOrigin: true,
+    })
+);
+
+// Root
+app.get("/", (req, res) => {
+    res.send("API Gateway Running");
+});
+
+app.listen(3000, () => {
+    console.log("Gateway running on port 3000");
+});
